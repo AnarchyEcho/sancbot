5a0fb8dc5406d4a967d7354d46437a53fd8b1dbe
EchoDev <anarchytrex@hotmail.com>
2026-08-28 23:40:59 +0200

    more devops

--- medusae/publish.sh
+++ medusae/publish.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+cd ../nadekopi || exit
+export "$(xargs <.env.local)"
+cd ./medusae || exit
 
 if [ "$1" = "" ]; then
   echo "Missing folder name"
@@ -6,24 +9,23 @@ if [ "$1" = "" ]; then
   exit
 fi
 
-pwd
-cd /home/funfun/Desktop/nadekopi/medusae/"$1" || exit
+cd /home/"$USERNAME"/Desktop/nadekopi/medusae/"$1" || exit
 
-dotnet publish -o /home/funfun/Desktop/nadekopi/medusae/"$1"/bin/medusae/"$1" /p:DebugType=embedded
+dotnet publish -o /home/"$USERNAME"/Desktop/nadekopi/medusae/"$1"/bin/medusae/"$1" /p:DebugType=embedded
 
 echo "Renaming dll to correct file name"
-mv /home/funfun/Desktop/nadekopi/medusae/"$1"/bin/medusae/"$1"/medusae.dll bin/medusae/"$1"/"$1".dll
+mv /home/"$USERNAME"/Desktop/nadekopi/medusae/"$1"/bin/medusae/"$1"/medusae.dll bin/medusae/"$1"/"$1".dll
 
-if [ -d "/home/funfun/Desktop/nadekopi/data/medusae/$1" ]; then
+if [ -d "/home/$USERNAME/Desktop/nadekopi/data/medusae/$1" ]; then
   echo "Removed old symlink"
-  rm -rf /home/funfun/Desktop/nadekopi/data/medusae/"$1"
+  rm -rf /home/"$USERNAME"/Desktop/nadekopi/data/medusae/"$1"
 fi
 
-cd /home/funfun/Desktop/nadekopi/medusae/"$1"/bin/medusae/"$1" || exit
+cd /home/"$USERNAME"/Desktop/nadekopi/medusae/"$1"/bin/medusae/"$1" || exit
 for file in *; do
   [[ $file = *.yml || $file = *.json || $file = "$1.dll" ]] && continue
   rm -rf "$file"
 done
 
 echo "Moving folder"
-mv /home/funfun/Desktop/nadekopi/medusae/"$1"/bin/medusae/"$1" /home/funfun/Desktop/nadekopi/data/medusae
+mv /home/"$USERNAME"/Desktop/nadekopi/medusae/"$1"/bin/medusae/"$1" /home/"$USERNAME"/Desktop/nadekopi/data/medusae
